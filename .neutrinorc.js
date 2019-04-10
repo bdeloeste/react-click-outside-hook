07da60d46ef6a61543520a703e76ee9b2557c371
Bryan Deloeste <bryandeloeste@gmail.com>
2019-04-10 14:57:43 -0700

    wip: set up repo

---
+++ .neutrinorc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  use: [
+    [
+      '@neutrinojs/airbnb',
+      {
+        eslint: {
+          plugins: ['react-hooks'],
+          rules: {
+            semi: ['off'],
+            'import/prefer-default-export': ['off'],
+            'import/no-extraneous-dependencies': ['off'],
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'react/prop-types': ['off'],
+            'react/no-unused-prop-types': ['off'],
+            'react/forbid-prop-types': ['off'],
+            'react/require-default-props': ['off'],
+            'consistent-return': 0,
+          },
+        },
+      },
+    ],
+    '@neutrinojs/react-components',
+    [
+      '@neutrinojs/jest',
+      {
+        setupTestFrameworkScriptFile: '<rootDir>/test/setupTests.js',
+      },
+    ],
+  ],
+}
