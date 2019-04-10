a425e56c3ba18ec32183d2ff02c5246ab02b032b
Bryan Deloeste <bryandeloeste@gmail.com>
2019-04-10 14:59:03 -0700

    test: implement tests

---
+++ test/useClickOutside.test.jsx
@@ -0,0 +1,63 @@
+import { mount } from 'enzyme'
+import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import { useClickOutside } from '../src/components/useClickOutside'
+
+function ClickableComponent() {
+  const [ref, hasClickedOutside] = useClickOutside()
+  return (
+    <div id="clickable" ref={ref}>
+      {hasClickedOutside.toString()}
+    </div>
+  )
+}
+
+function Container() {
+  return (
+    <div>
+      <ClickableComponent />
+      <div id="dummy" />
+    </div>
+  )
+}
+
+function render() {
+  return mount(<Container />)
+}
+
+describe('useClickOutside', () => {
+  const map = {}
+  beforeAll(() => {
+    document.addEventListener = jest.fn((event, cb) => {
+      map[event] = cb
+    })
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('should click outside of the clickable element and set hasOutsideClicked to `true`', () => {
+    const wrapper = render()
+    const dummy = wrapper.find('#dummy')
+    act(() => {
+      map.mousedown({
+        target: ReactDOM.findDOMNode(dummy.instance()),
+      })
+    })
+    const clickable = wrapper.find('#clickable')
+    expect(clickable.text()).toEqual('true')
+  })
+
+  it('should click inside of the clickable element and set hasOutsideClicked to `false`', () => {
+    const wrapper = render()
+    const dummy = wrapper.find('#dummy')
+    const clickable = wrapper.find('#clickable')
+    act(() => {
+      map.mousedown({
+        target: ReactDOM.findDOMNode(clickable.instance()),
+      })
+    })
+    expect(clickable.text()).toEqual('false')
+  })
+})
